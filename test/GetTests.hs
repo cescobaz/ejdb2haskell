968d8b8a092573c416ef0dc91bec03c03a20aba1
Francesco Burelli <francesco.burelli@gmail.com>
2020-03-22 23:22:13 +0100

    getById test improved

--- test/GetTests.hs
+++ test/GetTests.hs
@@ -1,11 +1,24 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module GetTests ( tests ) where
 
-import qualified Data.HashMap.Strict as Map
+import           Data.Aeson       ( FromJSON )
 
 import           Database.EJDB2
 
+import           GHC.Generics
+
 import           Test.Tasty
 import           Test.Tasty.HUnit
+
+data Plant = Plant { name        :: Maybe String
+                   , isTree      :: Maybe Bool
+                   , year        :: Maybe Int
+                   , description :: Maybe String
+                   }
+    deriving ( Eq, Generic, Show )
+
+instance FromJSON Plant
 
 tests :: TestTree
 tests = testGroup "get" [ getByIdAndParseToMap ]
@@ -19,5 +32,9 @@ getByIdAndParseToMap = withResource (open testReadOnlyDatabaseOpts) (close) $
         database <- databaseIO
         plant <- getById database "plants" 1
         putStrLn $ show plant
-        plant @?= (Just (Map.fromList [ ("name", "pinus") ]))
+        plant @?= (Just Plant { name        = Just "pinus"
+                              , isTree      = Just True
+                              , year        = Just 1753
+                              , description = Just "wow 🌲"
+                              })
 
---
+++ test/read-only-db-maker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl http://localhost:9191/plants -d '{"name":"pinus","isTree":true,"year":1753,"description":"wow 🌲"}'
