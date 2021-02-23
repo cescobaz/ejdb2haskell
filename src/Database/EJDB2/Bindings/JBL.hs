ca7ab06a9ec3977a430a69900ea4b450d02469b6
Francesco Burelli <francesco.burelli@gmail.com>
2021-02-23 21:44:45 +0100

    JBL binding improved with new functions

--- src/Database/EJDB2/Bindings/JBL.hs
+++ src/Database/EJDB2/Bindings/JBL.hs
@@ -2,6 +2,7 @@
 
 module Database.EJDB2.Bindings.JBL where
 
+import           Database.EJDB2.JBLIterator
 import           Database.EJDB2.Result
 
 import           Foreign
@@ -10,13 +11,13 @@ import           Foreign.C.Types
 
 type JBL = Ptr ()
 
-type JBLIterator = Ptr ()
-
 type JBLNode = Ptr ()
 
 type JBLPrintFlags = CUChar
 
 type JBLJSONPrinter = Ptr CChar -> CInt -> CChar -> CInt -> Ptr () -> IO RC
+
+type JBLTypeT = CInt
 
 foreign import ccall "wrapper" mkJBLJSONPrinter
     :: JBLJSONPrinter -> IO (FunPtr JBLJSONPrinter)
@@ -64,7 +65,25 @@ foreign import ccall "ejdb2/jbl.h jbl_create_iterator_holder" c_jbl_create_itera
     :: Ptr JBL -> IO RC
 
 foreign import ccall "ejdb2/jbl.h jbl_iterator_init" c_jbl_iterator_init
-    :: JBL -> Ptr JBLIterator -> IO RC
+    :: JBL -> JBLIteratorPtr -> IO RC
 
 foreign import ccall "ejdb2/jbl.h jbl_iterator_next" c_jbl_iterator_next
-    :: Ptr JBLIterator -> JBL -> Ptr CString -> Ptr CInt -> IO CBool
+    :: JBLIteratorPtr -> JBL -> Ptr CString -> Ptr CInt -> IO CBool
+
+foreign import ccall "ejdb2/jbl.h jbl_object_get_type" c_jbl_object_get_type
+    :: JBL -> CString -> IO JBLTypeT
+
+foreign import ccall "ejdb2/jbl.h jbl_object_get_i64" c_jbl_object_get_i64
+    :: JBL -> CString -> Ptr CIntMax -> IO RC
+
+foreign import ccall "ejdb2/jbl.h jbl_object_get_f64" c_jbl_object_get_f64
+    :: JBL -> CString -> Ptr CDouble -> IO RC
+
+foreign import ccall "ejdb2/jbl.h jbl_object_get_bool" c_jbl_object_get_bool
+    :: JBL -> CString -> Ptr CBool -> IO RC
+
+foreign import ccall "ejdb2/jbl.h jbl_object_get_str" c_jbl_object_get_str
+    :: JBL -> CString -> Ptr CString -> IO RC
+
+foreign import ccall "ejdb2/jbl.h jbl_object_get_fill_jbl" c_jbl_object_get_fill_jbl
+    :: JBL -> CString -> JBL -> IO RC
